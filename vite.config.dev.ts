8cee0be2469223ce2368f596cacfc2b5fb4f9b7b
Kiyamov Bulat <kiyamovit@gmail.com>
2025-11-04 19:28:06 +0300

    MPV: add: demo:build (update: vite.config.dev.ts (add: build.outDir))

--- vite.config.dev.ts
+++ vite.config.dev.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
 
 export default defineConfig({
     root: 'demo', // указываем корень демо-приложения
     plugins: [react()],
+    build: {
+        outDir: path.join(__dirname, 'demo_dist'),
+    },
 });
